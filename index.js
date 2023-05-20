724813e7eea047b0e1abce248747c31ad5ef22e5
Samiul <samiultalukder@gmail.com>
2023-05-20 23:35:04 +0600

    Varcel Deployment

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpxgr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpxgr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// console.log(process.env.DB_USER);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpxgr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -108,3 +111,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("Listening Port:", port);
 });
+
+
+module.exports = app;
